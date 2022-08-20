155185bb442707a619effd57f746589c7a3ee46d
wuzhenghong <2015129064@qq.com>
2022-08-20 14:01:20 +0800

    feat: handleDates

---
+++ utils/handleDates.js
@@ -0,0 +1,33 @@
+const dayjs = require('dayjs').default
+const weekday = require('dayjs/plugin/weekday').default
+
+dayjs.extend(weekday)
+
+export function formatDate(date, dateFormat = 'YYYYMMDD') {
+  return dayjs(date).format(dateFormat)
+}
+
+const weekdays = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六']
+export function getWeekday(date) {
+  const index = dayjs(date).weekday()
+  return weekdays[index]
+}
+
+export function addDays(date, days = 1) {
+  const day = dayjs(date).add(days, 'day')
+  return new Date(day.toDate())
+}
+
+export function subDays(date, days = 1) {
+  const day = dayjs(date).subtract(days, 'day')
+  return new Date(day.toDate())
+}
+
+export function getYearRange() {
+  const minDate = dayjs().subtract(2, 'year')
+  const maxDate = dayjs().add(2, 'year')
+  return {
+    minDate: minDate.toDate().getTime(),
+    maxDate: maxDate.toDate().getTime()
+  }
+}
