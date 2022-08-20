a120c3a57645c65743f2abd51bc6738788da1c33
wuzhenghong <2015129064@qq.com>
2022-08-20 22:57:43 +0800

    feat: logo-icon

---
+++ components/logo-icon/index.js
@@ -0,0 +1,30 @@
+// components/logo-icon/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    name: {
+      type: String,
+      value: ''
+    },
+    size: {
+      type: String,
+      value: '32rpx'
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
