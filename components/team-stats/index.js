f243023ab00398eefd668eb4700682340b9cb5b7
wuzhenghong <2015129064@qq.com>
2022-08-26 10:27:45 +0800

    feat: team-stats

---
+++ components/team-stats/index.js
@@ -0,0 +1,31 @@
+// components/team-stats/index.js
+import { TEAM_NAME } from '../../common/constants'
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    gameSummary: {
+      type: Array,
+      value: () => []
+    },
+    allTeamStats: {
+      type: Array,
+      value: () => []
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    TEAM_NAME
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
