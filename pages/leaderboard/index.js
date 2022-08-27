b154b3b6303928cf500ee2980726f2e755913278
wuzhenghong <2015129064@qq.com>
2022-08-27 17:50:18 +0800

    feat: leaderboard page

--- pages/leaderboard/index.js
+++ pages/leaderboard/index.js
@@ -1,18 +1,57 @@
 // pages/leaderboard/index.js
+import { getStandings } from '../../api/index'
+import { EAST_TEAMS, WEST_TEAMS } from '../../common/constants'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    eastTeams: [],
+    westTeams: [],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad(options) {
-
+  async onLoad(options) {
+    const { data } = await getStandings()
+    const { teams } = data.league.standard
+    this.genLeaderboard(teams)
+  },
+  // 构造数据
+  genLeaderboard(teams) {
+    const pick = this.sort(teams)
+    const eastTeams = this.addWinDiff(pick(EAST_TEAMS))
+    const westTeams = this.addWinDiff( pick(WEST_TEAMS))
+    this.setData({
+      eastTeams,
+      westTeams
+    })
+  },
+  // 胜差
+  addWinDiff(teams) {
+    console.log(teams)
+    const beseLine = teams[0].win
+    return teams.map(t => {
+      return {
+        ...t,
+        winDiff: beseLine - t.win
+      }
+    })
+  },
+  // 排序
+  sort(allTeams) {
+    return (subTeams) => subTeams.map(tId => {
+      const item = allTeams.find(t => t.teamId === tId)
+      return {
+        triCode: item.teamSitesOnly.teamTricode,
+        win: item.win,
+        loss: item.loss,
+        winPct: item.winPct,
+        winPctV2: item.winPctV2+'%'
+      }
+    }).sort((a, b) => b.winPct - a.winPct)
   },
 
   /**
